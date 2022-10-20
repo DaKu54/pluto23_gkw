c1c327bbd13d02511c98e3d3313ff36587b4a7b8
Prof-Greipl <dieter.greipl@haw-landshut.de>
2022-10-20 15:21:16 +0200

    class after T1 (incl. Actvity-Cycle)

--- app/src/main/java/de/hawlandshut/pluto23_gkw/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto23_gkw/MainActivity.java
@@ -15,4 +15,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate called");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart called");
+    }
 }
